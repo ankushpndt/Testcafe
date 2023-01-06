8831a554ab6dafe22f178148cb377ee9ac467417
Ankush Sharma <ankush.s@smarter.codes>
2023-01-06 14:50:36 +0530

    add: take screenshot

--- homePage.ts
+++ homePage.ts
@@ -2,8 +2,7 @@ import { Selector } from 'testcafe'; // first import testcafe selectors
 import { ClientFunction } from 'testcafe';
 
 fixture `Home Page`// declare the fixture
-    .page `https://dev.fw-dts.vercel.app/`;  // specify the start page
-
+    .page `https://dev.fw-dts.vercel.app/`
 
 const scroll = ClientFunction(( behavior?: ScrollBehavior, top?:number, bottom?:number) => {
     window.scroll({
@@ -16,5 +15,9 @@ const scroll = ClientFunction(( behavior?: ScrollBehavior, top?:number, bottom?:
 test('home page test', async t => {
     await t.maximizeWindow()
     await scroll('smooth', 5997)
-    // await t.debug();
+    await t.wait(5000);
+    await t.takeScreenshot({
+        path: 'homepage.png',
+        fullPage: true,
+    })
 });

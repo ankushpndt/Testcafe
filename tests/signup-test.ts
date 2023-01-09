9c4351d475376f7cc202d9b4f4a88777ccafaf9a
ankush.s212 <ankush.s@smarter.codes>
2023-01-09 20:14:46 +0530

    add: signup testcase

---
+++ tests/signup-test.ts
@@ -0,0 +1,48 @@
+import { Selector } from 'testcafe'; // first import testcafe selectors
+import { ClientFunction } from 'testcafe';
+
+fixture `SignIn`// declare the fixture
+    .page `https://dev.fw-dts.vercel.app/`;
+
+const scroll = ClientFunction(( behavior?: ScrollBehavior, top?:number, bottom?:number) => {
+    window.scroll({
+        top,
+        behavior
+    });
+});
+
+const userIcon = Selector('.nav_wrapper > div > div > div > div')
+const signUpLink = Selector('a').withAttribute('href', '/register')
+const firstNameInput = Selector('input').withAttribute('name', 'firstName')
+const lastNameInput = Selector('input').withAttribute('name', 'lastName')
+const emailInput = Selector('input').withAttribute('name', 'email')
+const mobileNumberInput = Selector('input').withAttribute('name', 'mobileNumber')
+const passwordInput = Selector('input').withAttribute('name', 'password')
+const passwordConfirmationInput = Selector('input').withAttribute('name', 'reEnterPassword')
+const checkboxBtn = Selector('input').withAttribute('name', 'isEmailEnabled')
+const createAnAccountButton = Selector('button').withText('Create an account')
+const signInBtn = Selector('button').withText('Sign in')
+const logoutLogo = Selector('.nav_wrapper > div > div > div > a > div').withText('AS')
+const logoutBtn = Selector('button > div > span').withText('Sign out')
+
+//then create a test and place your code there
+test('SignIn test', async t => {
+    await t.maximizeWindow()
+    await t.click(userIcon)
+    .click(signUpLink)
+    .typeText(firstNameInput, 'Test')
+    .typeText(lastNameInput, 'User')
+    .typeText(emailInput, 'test2@gmail.com')
+    .typeText(mobileNumberInput, '0933726111')
+    .typeText(passwordInput, 'Testuser@123')
+    .typeText(passwordConfirmationInput, 'Testuser@123')
+    .click(checkboxBtn)
+    .click(createAnAccountButton)
+    await t.wait(5000)
+    await t.takeScreenshot({
+        path: 'signin.png',
+        fullPage: true,
+    })
+    // await t.click(logoutLogo).wait(5000)
+    // await t.click(logoutBtn)
+});
